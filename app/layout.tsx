381c04527ee39c6d6a1e5bfc9ff4e30261f36ec1
said <said.farhi72@gmail.com>
2024-06-29 19:48:16 +0100

    first push

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "react-calendar/dist/Calendar.css";
+
+import { siteConfig } from "@/config/site";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
+import { Toaster } from "sonner";
+import { ModalProvider } from "@/providers/modal-provider";
+import { ModalProviderTril } from "@/components/providers/modal-provider";
+import { Suspense } from "react";
+import { Loading } from "@/components/auth/loading";
+import { LoadingModal } from "@/components/modals/loding-modal";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: [
+    {
+      url: "/log.png",
+      href: "/log.png",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" data-theme={"bumblebee"} className="h-full bg-base-100">
+      <ConvexClientProvider>
+        <QueryProvider>
+          <Toaster />
+          <ModalProviderTril />
+          <ModalProvider />
+
+          <body className={inter.className}>{children}</body>
+        </QueryProvider>
+      </ConvexClientProvider>
+    </html>
+  );
+}
